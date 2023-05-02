be2c463a0a608d3e97b091b8ffd6ab94f4a1479d
Graeme Paterson <ghpaterson@gmail.com>
2023-05-02 11:47:07 +0100

    rendered api data yo pages

--- app/page.js
+++ app/page.js
@@ -5,9 +5,40 @@ import Link from "next/link";
 import arrowwhite from "../public/arrow-white.svg";
 import About from "@/components/about";
 import Projects from "@/components/projects";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { GraphQLClient, gql } from "graphql-request";
+
+const graphcms = new GraphQLClient(
+  "https://api-eu-west-2.hygraph.com/v2/clh3jd99j56t501t8644q7je9/master"
+);
+
+const QUERY = gql`
+  {
+    posts {
+      id
+      title
+      slug
+      content {
+        html
+      }
+      photo {
+        url
+      }
+    }
+  }
+`;
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const { posts } = await graphcms.request(QUERY);
+      setPosts(posts);
+    }
+    fetchPosts();
+  }, []);
+
   const headlessURL = "https://headless-iota-five.vercel.app/";
 
   const aboutRef = useRef(null);
@@ -20,29 +51,48 @@ export default function Home() {
       behavior: "smooth",
     });
   };
+
+  const heroPost = posts.find(
+    (post) =>
+      post.photo.url === "https://media.graphassets.com/7cCxxEsRRiNqJkCBHe7g"
+  );
+
   return (
     <main className="bg-blu h-full w-screen" ref={homeRef}>
       <section>
-        <ul className="flex flex-col font-fungis text-9xl text-white pt-32 ml-20">
-          <div
-            className="hover:cursor-pointer"
-            onClick={() => scrollToElement(aboutRef)}
-          >
-            ABOUT
-          </div>
-          <div
-            className="hover:cursor-pointer"
-            onClick={() => scrollToElement(projectsRef)}
-          >
-            PROJECTS
-          </div>
-          <div className="flex items-center gap-10">
-            <Link href={headlessURL} target="_blank">
-              BLOG
-            </Link>
-            <Image src={arrowwhite} width={75} className="-mt-6" />
-          </div>
-        </ul>
+        <div className="flex relative">
+          <ul className="flex flex-col font-fungis text-9xl text-white pt-32 ml-24 z-10">
+            <div
+              className="hover:cursor-pointer"
+              onClick={() => scrollToElement(aboutRef)}
+            >
+              ABOUT
+            </div>
+            <div
+              className="hover:cursor-pointer"
+              onClick={() => scrollToElement(projectsRef)}
+            >
+              PROJECTS
+            </div>
+            <div className="flex items-center gap-10">
+              <Link href={headlessURL} target="_blank">
+                BLOG
+              </Link>
+              <Image src={arrowwhite} width={75} className="-mt-6" />
+            </div>
+          </ul>
+          {heroPost && (
+            <div className="z-0 mt-8 -ml-12">
+              <img
+                src={heroPost.photo.url}
+                width={400}
+                height={400}
+                alt="photo"
+                className=""
+              />
+            </div>
+          )}
+        </div>
       </section>
       <section>
         <div className="flex justify-center my-10 ml-32 font-fungis text-3xl text-white">
--- components/projects.js
+++ components/projects.js
@@ -2,23 +2,23 @@ import NavBar from "@/components/navbar";
 import Smiley from "@/components/smiley";
 import Link from "next/link";
 
-export default function Projects({ homeRef }) {
+export default function Projects({ homeRef, id, title, content, photo, slug }) {
   return (
     <main className="h-full bg-blu">
       <NavBar homeRef={homeRef} />
       <section className="h-full bg-blu">
         <div className="flex justify-center">
           <ul className="flex flex-col font-fungis text-8xl text-white gap-2">
-            <Link className="hover:underline" href="#">
+            <Link className="hover:underline" href={"/posts/" + slug}>
               ECOLIDAY
             </Link>
-            <Link className="pl-8 hover:underline" href="#">
+            <Link className="pl-8 hover:underline" href={"/posts/" + slug}>
               MR BLOGGY
             </Link>
-            <Link className="pl-16 hover:underline" href="#">
+            <Link className="pl-16 hover:underline" href={"/posts/" + slug}>
               DECODE CARE
             </Link>
-            <Link className="pl-24 hover:underline" href="#">
+            <Link className="pl-24 hover:underline" href={"/posts/" + slug}>
               HEADLESS
             </Link>
           </ul>
