2ed4e48ac6fb65e06d0a55b9974cd435882b6d3e
Graeme Paterson <ghpaterson@gmail.com>
2023-05-04 11:26:23 +0100

    hero section css

--- app/page.js
+++ app/page.js
@@ -5,10 +5,17 @@ import Projects from "@/components/projects";
 import Hero from "@/components/hero";
 
 export default function Home({ posts }) {
+  const scrollToElement = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <main>
       <div id="hero">
-        <Hero />
+        <Hero scrollToElement={scrollToElement} />
       </div>
       <div id="about">
         <About />
--- components/hero.js
+++ components/hero.js
@@ -1,3 +1,5 @@
+import { BsArrowsFullscreen } from "react-icons/bs";
+
 import Link from "next/link";
 
 const API_KEY = process.env.API_KEY;
@@ -34,22 +36,15 @@ const getPosts = async () => {
   return data.posts;
 };
 
-export default async function Hero({ heroPost }) {
+export default async function Hero({ heroPost, scrollToElement }) {
   const posts = await getPosts();
   const headlessURL = "https://headless-iota-five.vercel.app/";
 
   const postId = "clh4tmqs68bvw0bmk45lbiouy";
   const post = posts.find((post) => post.id === postId);
 
-  const scrollToElement = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
   return (
-    <main className="bg-blu h-full w-screen">
+    <main className="bg-blu h-screen w-screen">
       <section>
         <div className="flex relative">
           <ul className="flex flex-col font-fungis text-9xl text-white pt-32 ml-24 z-10">
@@ -69,19 +64,24 @@ export default async function Hero({ heroPost }) {
               <Link href={headlessURL} target="_blank">
                 BLOG
               </Link>
+              <div className="text-8xl -mt-6">
+                <BsArrowsFullscreen />
+              </div>
             </div>
           </ul>
-          {post && (
-            <div key={post.id}>
-              {post.photo && (
-                <img src={post.photo.url} width={300} height={300} />
-              )}
-            </div>
-          )}
+          <div className="z-0 -ml-10 mt-6">
+            {post && (
+              <div key={post.id}>
+                {post.photo && (
+                  <img src={post.photo.url} width={400} height={400} />
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </section>
       <section>
-        <div className="flex justify-center my-10 ml-32 font-fungis text-3xl text-white">
+        <div className="flex justify-center my-12 ml-32 font-fungis text-3xl text-white">
           <div>
             <p>Graeme Paterson. Software Developer.</p>
             <p className="ml-8">Career Switcher. London. UK</p>
