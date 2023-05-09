c20413dd44a0160f1dddca126bc618e40c8632ce
Graeme Paterson <ghpaterson@gmail.com>
2023-05-09 15:53:59 +0100

    trying to fix indiv projects page

--- projects/ecoliday.js
+++ projects/ecoliday.js
@@ -6,11 +6,10 @@ export default async function Ecoliday() {
   return (
     <main>
       <section>
-        <div className="flex flex-col  items-end gap-2 ml-10 text-blu font-fungis">
+        <div className="flex gap-2 ml-10 text-blu font-fungis">
           <Link href={ecolidayURL} target="_blank">
-            <h2 className="text-5xl text-blu -mt-10">ECOLIDAY</h2>
+            <h2 className="text-6xl text-blu -mt-10">ECOLIDAY</h2>
           </Link>
-          <h2 className="text-4xl">'click'</h2>
         </div>
       </section>
     </main>
